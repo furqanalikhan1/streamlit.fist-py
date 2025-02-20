24ee9ef8ac181ae0b50dd578c3d551190067c505
Furqan Khan <160186238+furqanalikhan1@users.noreply.github.com>
2025-02-21 03:48:05 +0500

    Update main.py

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ if uploaded_files:
 
             with col2:
                 if st.button(f"Fill missing values for {file.name}"):
-                    numeric_cols =df.select_dtypes(includes=['number']). columns
+                    numeric_cols =df.select_dtypes(include=['number']). columns
                     df[numeric_cols] = df [numeric_cols].fillna(df [numeric_cols].mean())
                     st.write("Missing vallues have been filled")
 
